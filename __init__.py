515fffbeec14f977d8aa8b163c5912a9f306ba1e
nathan <nathanhigginbotham@mail.com>
2024-11-14 10:47:48 +0000

    Add: module initialisation and validation notebook

---
+++ __init__.py
@@ -0,0 +1,10 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+import scipy 
+import h5py # Import h5py for saving data
+
+from scipy.special import i0
+from scipy.integrate import quad
+from scipy import integrate
+from scipy.stats import rice, rayleigh
+
