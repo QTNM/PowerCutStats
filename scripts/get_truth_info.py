427122eec6fcb8af35560ec9d628ad34671637c0
nathan <nathanhigginbotham@mail.com>
2025-03-24 15:10:13 +0000

    Update: Truth info script

--- scripts/get_truth_info.py
+++ scripts/get_truth_info.py
@@ -7,12 +7,12 @@ from testing import *
 
 
 save_dir = '../data/simulations_truth/'
-save_name = 'TrueHeScatters_test3'
+save_name = 'GoldenSample'
 
 # path_to_dir = '/Users/nathan/Desktop/HeFractionScatters/TrueHeliumScatters/' # Path to data, this needs to be changed for your local data
 
 
-path_to_dir = "../../../../Desktop/TrueHeScatters/" # Path to data, this needs to be changed for your local data
+path_to_dir = "../../../../Desktop/GoldenSample/" # Path to data, this needs to be changed for your local data
 
 
 save_csv = '.csv'
@@ -35,6 +35,9 @@ if __name__ == "__main__":
     file_list = glob.glob(path_to_dir + '**/*.h5', recursive=True) # List of Files for within subdirectories
     
     data_container = []
+    
+    data_dict = None
+    initialised = False
 
     for i, f in enumerate(file_list):
   
@@ -47,9 +50,10 @@ if __name__ == "__main__":
             keys.insert(0, 'File_name')
             
                
-            if i == 0: # Initialise dictionary
+            if not initialised : # Initialise dictionary
             
                 data_dict = {key: [] for key in keys}
+                initialised = True
             
             for j, key in enumerate(keys):
                 if j == 0:
@@ -68,6 +72,10 @@ if __name__ == "__main__":
 
         except:
             print(f'Error in file {f}')
+            
+            if data_dict is not None:
+                for key in keys:
+                    data_dict[key].append(np.nan)
         
     df = pd.DataFrame(data_dict)
     
